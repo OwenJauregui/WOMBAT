203cf0fcacf8f3f7bcc451bdc6716d462b8d38bf
root <jauborowen@gmail.com>
2023-05-11 15:41:16 -0600

    Implemented control message and control node

--- ros_ws/src/navegation/src/control.py
+++ ros_ws/src/navegation/src/control.py
@@ -1,76 +1,64 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
-#ros
+#ROS dependencies
 import rospy
+from custom_msgs.msg import vel_cmd
 from geometry_msgs.msg import Pose2D
 
-#Puzzlebot
+#Import numpy
 import numpy as np
 
-def main():
+q  = np.array([[0, 0, 0]]).T
+qd = np.array([[0, 0, 0]]).T
 
-    kart = rospy.Publisher("/Puzzlebot/pose", Pose2D, queue_size = 10)
-    
-    rospy.init_node("Control")
-    rate = rospy.Rate(1000)
+#Parameters
+r = 0.04 #wheel radius
+d = 0.10 #distance between wheels
+h = 0.15 #distance between center and new point
 
-    #Parameters
-    dt = 0.001 #time step
+k = np.array([[1, 0],
+              [0, 2]])
+cmd = vel_cmd()
 
-    r = 0.04 #wheel radius
-    d = 0.10 #distance between wheels
-    h = 0.15 #distance between center and new point
+control = rospy.Publisher("/wombat/navegation/control", vel_cmd, queue_size = 10)
 
-    k = np.array([[1, 0],
-                  [0, 2]])
-
-    #             [X, Y, theta]
-    q = np.array([[0.0,0.0,0.0]]).T
-
-    qd = np.array([[1000.0, -500.0]]).T
+def controlCalc():
+    global q, qd, r, d, h, k, cmd, control
 
     qe = q[0:2] - qd
 
-    #control U = [Wr, Wl]
-    u = np.array([[0,0]]).T
-
-    #matrix
-    D = np.array([[r/2*np.cos(q[2,0]) - h*r/d*np.sin(q[2,0]), r/2*np.cos(q[2,0]) + h*r/d*np.sin(q[2,0])], 
-                  [r/2*np.sin(q[2,0]) + h*r/d*np.cos(q[2,0]), r/2*np.sin(q[2,0]) - h*r/d*np.cos(q[2,0])]])
-    
-    Phi = np.array([[r/d, -r/d]])
-
-    A = np.concatenate(D,Phi)
-
-    
-    pose = Pose2D()
-
-    while not rospy.is_shutdown():
-
-        qe = q[0:2] - qd
-        #print(qe)
-        u = np.linalg.inv(D)@-k@qe
-        #print(u)
-        
         #u = np.array([[1,5]]).T
-        D = np.array([[r/2*np.cos(q[2,0]) - h*r/d*np.sin(q[2,0]), r/2*np.cos(q[2,0]) + h*r/d*np.sin(q[2,0])], 
+    D = np.array([[r/2*np.cos(q[2,0]) - h*r/d*np.sin(q[2,0]), r/2*np.cos(q[2,0]) + h*r/d*np.sin(q[2,0])],
                   [r/2*np.sin(q[2,0]) + h*r/d*np.cos(q[2,0]), r/2*np.sin(q[2,0]) - h*r/d*np.cos(q[2,0])]])
+
+    u = np.dot(np.linalg.inv(D),np.dot(-k,qe))
+
+    cmd.wr.data = u[0, 0]
+    cmd.wl.data = u[1, 0]
+
+    control.publish(cmd)
+
+def newEstimation(pose_estimation):
+    global q
+    q[0,0] = pose_estimation.x
+    q[1,0] = pose_estimation.y
+    q[2,0] = pose_estimation.theta
+    controlCalc()
+
+def newGoal(goal_pose):
+    global qd
+    qd[0,0] = goal_pose.x
+    qd[1,0] = goal_pose.y
+    qd[2,0] = goal_pose.theta
+
+def main():
     
-        Phi = np.array([[r/d, -r/d]])
+    pose_sub = rospy.Subscriber("/wombat/navegation/estimation", Pose2D, newEstimation, queue_size = 10)
 
-        A = np.concatenate(D,Phi)
-
-        #print(A@u*dt)
-        q += A@u*dt
-
-        pose.x = q[0]
-        pose.y = q[1]
-        pose.theta = q[2]
-
-        kart.publish(pose)
-
-        rate.sleep()
-
+    goal_sub = rospy.Subscriber("/wombat/navegation/goal", Pose2D, newGoal, queue_size = 1)
+    rospy.init_node("Control")
+    rate = rospy.Rate(1000)
+    rospy.spin()
 
 if __name__ == "__main__":
     main()
